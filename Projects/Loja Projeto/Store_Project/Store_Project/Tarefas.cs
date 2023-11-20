2191ed8bff545e42199864b0f10f9f31cc99a1ec
LuciusPinhal <pinhal.lucius@gmail.com>
2023-11-20 18:13:55 -0300

    :fire: Criando arquivos para cada modal - Editar, Adcionar, Deletar

---
+++ Projects/Loja Projeto/Store_Project/Store_Project/Tarefas.cs
@@ -0,0 +1,19 @@
+﻿//
+//Editar loja - Alterar no banco de dados
+//deletar loja - Alterar no banco de dados
+
+//fazer verficações onde tem value se o usuario colocar string
+
+//Adcionar filtro E deixar o tiltulo no meio da tela
+//Filtro para filtrar nomes ou cidades
+//fazer compare preços - Table list valor mesmo produto valor do menor para maior
+
+
+//Falta arrumar ver Produtos
+
+
+
+
+//Perguntas para o prof
+
+//como fazer uma lista no formularo para enviar
